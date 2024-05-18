7bcdd6bcd7067cb89af118707d5d0eb9b535e162
Zffu <radi0o0@outlook.com>
2024-05-18 09:32:45 +0200

    Moved stuff into modules

---
+++ plugin/src/main/java/net/zffu/eznpcs/wrappers/INPCWrapper.java
@@ -0,0 +1,11 @@
+package net.zffu.eznpcs.wrappers;
+
+/**
+ * The Wrapper of all Packet related actions that depends on NMS.
+ * We use Wrappers interfaces to guarantee a seamingless experience when changing versions and unlike nms this doesn't cause optimization issues.
+ */
+public interface INPCWrapper {
+
+    void spawnHumanEntity(int entityId, )
+
+}
